da62dda3d880affc6a4b96ace9ec8a928f5d4048
Lahiru De Silva <lahirurashmitha00@gmail.com>
2025-05-30 23:27:52 +0530

    Install tailwindcss and Add navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'todo',
+        
+    },{
+        path: 'todo',
+        pathMatch: 'full',
+        loadComponent: () => {
+            return import('./home/home').then((module) => module.Home);
+        },
+    }
+];
---
+++ src/app/navbar/navbar.ts
@@ -0,0 +1,12 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css'
+})
+export class Navbar {
+  title = input('Todo');
+
+}
