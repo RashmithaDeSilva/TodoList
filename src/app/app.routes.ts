dc47b2ac8c13f1c12036d6217ba9c1e22743cae2
Lahiru De Silva <lahirurashmitha00@gmail.com>
2025-06-01 15:59:29 +0530

    Update router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [
-    {
+export const routes: Routes = [{
         path: '',
-        pathMatch: 'full',
-        redirectTo: 'todolist',
-        
-    },{
-        path: 'todolist',
         pathMatch: 'full',
         loadComponent: () => {
             return import('./home/home').then((module) => module.Home);
